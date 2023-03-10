5bfa742100f1f4180f9a3fda946d20f139af3622
EshuShango <tobiakinyemi21@gmail.com>
2023-03-10 00:40:09 -0600

    fxd User Model, User mdl fin

--- models/User.js
+++ models/User.js
@@ -1,24 +1,41 @@
 const { Schema, model } = require("mongoose");
+const userSchema = Schema(
+  {
+    usrNme: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})|([^\s@]+@[^\s@]+\.[^\s@]+)$/,
+    },
 
-const userSchema = Schema({
-  usrNme: {
-    type: String,
-    unique: true,
-    required: true,
-    trimmed: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})|([^\s@]+@[^\s@]+\.[^\s@]+)$/,
-  },
-  thoughts: {
-    type: [
+    thoughts: [
       {
-        type: Schema.type.ObjectId,
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
         ref: "User",
       },
     ],
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+userSchema.virtual("friendCount").get(() => {
+  return this.friends.length;
 });
+const User = model("User", userSchema);
+module.exports = User;
